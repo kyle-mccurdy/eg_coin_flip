04f8a66e0a39beb5d075a195e2d030fb29c36f66
kyle-mccurdy <mccurdy@iastate.edu>
2025-04-15 14:46:22 -0500

    new demo

---
+++ js/new.js
@@ -0,0 +1,24 @@
+let;
+let;
+let;
+function runGame(param) {
+    let someVariable = param;
+    let;
+    let;
+    if (something == 1) {       
+        'do stuff here';         
+      } else {                    
+        'do other stuff';         
+      };                          
+    
+      let;
+      if (this === that) {    
+        'do stuff here';      
+        'do more stuff';      
+      } else {                
+        'do another stuff';   
+        'and do more'         
+      };
+      'total of games played'++;
+      let lorem = document.getElementById('ipsum');
+      lorem.innerText = choiceName;}
